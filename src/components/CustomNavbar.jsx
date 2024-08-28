f00a55496fed2d622bc8566e0f1078b06bd61632
Stefano Casasola <ste.casasola@gmail.com>
2024-08-28 11:14:06 +0200

    updates navbar

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,17 +1,85 @@
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
+// il componente Link di react-router-dom altro non è che una speciale "ancora"
+// che porta l'utente verso una determinata rotta -SENZA- ricaricare il browser
 
 const CustomNavbar = function () {
+  // useLocation è un react-hook di react-router-dom che ci fornirà info
+  // sulla rotta corrente
+  // REGOLE DEGLI HOOKS
+  // 1) si possono utilizzare gli hooks solamente nei componenti React a FUNZIONE
+  // 2) l'invocazione dell'hook va fatta prima del return e al di fuori di
+  // cicli, condizioni, funzioni etc.
+
+  const location = useLocation() // nome classico utilizzato nella documentazione
+  console.log('OGGETTO LOCATION', location)
+
+  // location.pathname è il percorso corrente nella barra degli indirizzi
+
+  const [linkUrls, setLinkUrls] = useState([
+    { path: '/', name: 'Home' },
+    { path: '/menu', name: 'Menu' },
+    { path: '/booking', name: 'Prenotazioni' },
+    { path: '/contact', name: 'Contatti' },
+    { path: '/admin', name: 'Admin' },
+  ])
+
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
       <Container fluid>
-        <Navbar.Brand href="#home">Epistaurant</Navbar.Brand>
+        <Link to="/" className="text-decoration-none">
+          <Navbar.Brand>Epistaurant</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Menu</Nav.Link>
-            <Nav.Link href="#features">Prenotazioni</Nav.Link>
-            <Nav.Link href="#pricing">Dove siamo</Nav.Link>
-            <Nav.Link href="#pricing">Contattaci</Nav.Link>
+            {/* il Link a "/" deve ricevere anche la classe "active" 
+            se location.pathname === "/" */}
+            <Link
+              to="/"
+              className={
+                location.pathname === '/' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Home
+            </Link>
+            <Link
+              to="/menu"
+              className={`nav-link${
+                location.pathname === '/menu' ? ' active' : ''
+              }`}
+            >
+              Menu
+            </Link>
+            <Link
+              to="/booking"
+              className={`nav-link ${
+                location.pathname === '/booking' && ' active'
+              }`}
+            >
+              Prenotazioni
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contattaci
+            </Link>
+            <Link to="/admin" className="nav-link">
+              Admin
+            </Link>
+
+            {/* {linkUrls.map((linkObject) => {
+              return (
+                <Link
+                  key={linkObject.path}
+                  to={linkObject.path}
+                  className={`nav-link${
+                    location.pathname === linkObject.path ? ' active' : ''
+                  }`}
+                >
+                  {linkObject.name}
+                </Link>
+              )
+            })} */}
           </Nav>
         </Navbar.Collapse>
       </Container>
