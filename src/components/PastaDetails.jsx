5345c60390c0806e7591328e9db0effb755b6594
Stefano Casasola <ste.casasola@gmail.com>
2024-08-28 13:06:29 +0200

    Finishes Epistaurant!

--- src/components/PastaDetails.jsx
+++ src/components/PastaDetails.jsx
@@ -1,4 +1,12 @@
-import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Container,
+  Row,
+  Spinner,
+} from 'react-bootstrap'
 // ora questa pagina dettaglio funzionerà per qualsiasi pastasciutta
 // il componente PastaDetails viene montato in App.js su una rotta "parametrica",
 // ovvero sa che qualunque sarà il suo percorso al suo interno troverà un parametro
@@ -8,32 +16,89 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 // giusta e mostrarne i dettagli
 
 // 1) recuperiamo dalla barra degli indirizzi il valore corrente del parametro "pastaId"
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+import pastasciutte from '../data/menu.json'
+import { useEffect, useState } from 'react'
+import PastaReviews from './PastaReviews'
 
 const PastaDetails = () => {
   const params = useParams() // params.pastaId è il valore dell'id della pasta
+  const navigate = useNavigate()
   console.log('OGGETTO PARAMS', params)
+  // c'è una correlazione diretta tra i nomi delle proprietà dell'oggetto "params"
+  // e i nomi dei parametri inseriti nella rotta dinamica (dopo i :)
+
+  // predispongo lo stato per salvare la pasta che eventualmente troverò
+  const [pasta, setPasta] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
+  // ora dobbiamo recuperare la pasta corretta dal JSON grazie all'id
+  useEffect(() => {
+    // all'avvio del componente... cerchiamo la pasta giusta
+    // params.pastaId
+
+    const pastaObject = pastasciutte.find(
+      (pasta) => pasta.id.toString() === params.pastaId
+    )
+    // il .toString() serve a trasformare (momentaneamente) l'id di ogni pasta
+    // in una stringa in modo da compararlo alla stringa del parametro da URL
+
+    console.log('PASTA TROVATA', pastaObject)
+    if (!pastaObject) {
+      navigate('/not-found')
+      // gestiamo i casi limite
+    } else {
+      setPasta(pastaObject) // salviamo la pasta trovata nello state
+
+      setTimeout(() => {
+        setIsLoading(false)
+        // :)
+      }, 500)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <Container>
       <Row className="justify-content-center mt-3">
         <Col xs={12} md={8}>
-          <Card>
-            <Card.Img variant="top" src="https://placedog.net/500" />
-            <Card.Body>
-              <Card.Title>Nome pasta</Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-                iste ab est excepturi corrupti delectus soluta, facilis ut nihil
-                magni? Quas, nesciunt? Possimus explicabo ducimus modi iure vero
-                repellendus asperiores.
-                {/* anche il prezzo */}
-              </Card.Text>
-              <Button variant="primary">Torna in Homepage</Button>
-            </Card.Body>
-          </Card>
+          {isLoading ? (
+            <div className="text-center">
+              <Spinner animation="border" variant="warning" />
+            </div>
+          ) : (
+            <Card>
+              <Card.Img variant="top" src={pasta.image} />
+              <Card.Body>
+                <Card.Title>{pasta.name}</Card.Title>
+                <Card.Text>
+                  {pasta.description}
+                  <div className="text-center my-2 p-3 fs-3">
+                    <Badge bg="success">{pasta.price}€</Badge>
+                  </div>
+                </Card.Text>
+                <div className="text-center">
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      navigate('/')
+                    }}
+                  >
+                    Torna in Homepage
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          )}
         </Col>
       </Row>
+      {!isLoading && (
+        <Row className="justify-content-center mt-3">
+          <Col xs={12} md={8}>
+            <PastaReviews activePasta={pasta} />
+          </Col>
+        </Row>
+      )}
       {/* todo: inserire anche i commenti della pasta */}
     </Container>
   )
